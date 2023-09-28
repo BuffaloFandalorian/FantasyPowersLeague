87a7aa8f0ef912c5a25062041dde62f28296c9e2
Stu Woodside <Stu.Woodside@towerswatson.com>
2023-09-27 23:38:41 -0400

    Added more auth checks

--- Models/AppIdentity.cs
+++ Models/AppIdentity.cs
@@ -5,5 +5,7 @@ namespace FantasyPowersLeague.Models
         public string Credential { get;set; }
         public string SessionId { get; set; }
         public bool TokenIsValid { get;set; }
+        public DateTime ExpirationDateTime { get;set; }
+        public DateTime IssueDateTime { get; set; } 
     }
 }
--- Services/IdentityService.cs
+++ Services/IdentityService.cs
@@ -14,7 +14,21 @@ namespace FantasyPowersLeague.Services
         {
             //validate token
             GoogleJsonWebSignature.Payload payload =  await GoogleJsonWebSignature.ValidateAsync(googleLoginDto.Credential);
-            return new AppIdentity { TokenIsValid = (payload != null), Credential = googleLoginDto.Credential, SessionId = Guid.NewGuid().ToString()};
+            var appIdentity = new AppIdentity 
+                { 
+                    TokenIsValid = (payload != null), 
+                    Credential = googleLoginDto.Credential, 
+                    SessionId = Guid.NewGuid().ToString(),
+                    ExpirationDateTime = DateTimeOffset.FromUnixTimeSeconds(payload?.ExpirationTimeSeconds ?? 0).DateTime,
+                    IssueDateTime = DateTimeOffset.FromUnixTimeSeconds(payload?.IssuedAtTimeSeconds ?? 0).DateTime
+                };
+            
+            if(appIdentity.ExpirationDateTime >= DateTime.UtcNow)
+            {
+                throw new Exception("Token is Expired");
+            }
+
+            return appIdentity;
         }
     }
 }
