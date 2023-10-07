72ed429236ff533f4a862fd00d80e888469c9e5b
Stu Woodside <buffalofandalorian@gmail.com>
2023-10-07 12:00:13 -0400

    convert nav menu to functional component, add nav

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,64 +1,58 @@
-import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Collapse, DropdownItem, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, Nav } from 'reactstrap';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../images/logo.png';
 import LoginWithGoogle from './google/GoogleLogin';
 import constants from '../models/constants';
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+const NavMenu = (props) => {
 
-  constructor (props) {
-    super(props);
+  const [collapsed, setCollapsed] = useState(true);
+  const navigate = useNavigate();
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.loginCallback = this.loginCallback.bind(this);
-    this.logoutCallback = this.logoutCallback.bind(this);
-
-    this.state = {
-      collapsed: true,
-      loggedInState : this.props.loggedInState
-    };
+  const toggleNavbar = () => {
+    setCollapsed(!collapsed);
   }
 
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  loginCallback = async (response) => {
-    this.setState({ loggedInState: await this.props.loginCallback(response)});
-  }
-
-  logoutCallback = async () => {
-    this.setState({ loggedInState: await this.props.logoutCallback()});
-  }
-
-  render() {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm  border-bottom box-shadow mb-3" container color="primary">
-          <NavbarBrand tag={Link} to="/"><img
-                      alt="logo"
-                      src={logo}
-                      style={{
-                        height: 50,
-                        width: 50
-                      }}
-          /> Fantasy Powers League</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-            <ul className="navbar-nav flex-grow">
-              <NavItem>
-                {(this.props.loggedInState === constants.LoginState.LoggedOut) && <LoginWithGoogle onSuccessfulLogin={this.loginCallback} /> }
-                {(this.props.loggedInState === constants.LoginState.LoggedIn) && <NavLink onClick={()=> this.logoutCallback()}>Logout</NavLink>}
-                {(this.props.loggedInState === constants.LoginState.LoggingIn) && <NavLink >Logging In...</NavLink>}
-              </NavItem>
+  return (
+    <header>
+      <Navbar className="navbar-expand-sm navbar-toggleable-sm  border-bottom box-shadow mb-3" container color="primary">
+        <NavbarBrand tag={Link} to="/"><img
+          alt="logo"
+          src={logo}
+          style={{
+            height: 50,
+            width: 50
+          }}
+        /> Fantasy Powers League</NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!collapsed} navbar>
+          <Nav className="me-auto" navbar>
+          <ul className="navbar-nav flex-grow">
+          {(props.loggedInState === constants.LoginState.LoggedIn) &&
+            <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Leagues
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem onClick={() => navigate("/new-league")}>New</DropdownItem>
+              <DropdownItem>Join</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem header>My Leagues</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>}
+            {(props.loggedInState === constants.LoginState.LoggedIn) && <NavItem><NavLink onClick={() => navigate("/")}>Home</NavLink></NavItem>}
+            <NavItem>
+              {(props.loggedInState === constants.LoginState.LoggedOut) && <LoginWithGoogle onSuccessfulLogin={props.loginCallback} />}
+              {(props.loggedInState === constants.LoginState.LoggedIn) && <NavLink onClick={props.logoutCallback}>Logout</NavLink>}
+              {(props.loggedInState === constants.LoginState.LoggingIn) && <NavLink >Logging In...</NavLink>}
+            </NavItem>
             </ul>
-          </Collapse>
-        </Navbar>
-      </header>
-    );
-  }
+            </Nav>
+        </Collapse>
+      </Navbar>
+    </header>
+  );
 }
+
+export default NavMenu;
